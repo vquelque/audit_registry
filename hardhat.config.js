8857a412bd06517a520ee9ffb687e98c00ce9142
Dominik Muhs <dmuhs@protonmail.ch>
2023-10-16 13:15:40 +0200

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,31 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/77ef266e244b47779bd0361a1dcab124",
+      accounts: ["3c3545cee9332dd2be1ff2d014eb188d84d8023564225bf3add050af4efcf04b"]
+    }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
