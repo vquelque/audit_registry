090a8aae052f4f7340c410dd95e9881e348a4766
Valentin <valentin.quelquejay@consensys.net>
2023-10-19 17:24:49 +0200

    update address

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
 export const SEPOLIA_CHAIN_ID = 11155111;
 export const SEPOLIA_RPC = "https://rpc.sepolia.org";
 export const SUPPORTED_NETWORKS = [SEPOLIA_CHAIN_ID];
-export const REGISTRY_ADDRESS = "0x36edbF7A96f20893972fD9b4cCd3845Be61819Fb";
+export const REGISTRY_ADDRESS = "0x3a92440039Dec1765bc93b95aC74636d3616fb72";
 export const INFURA_KEY = "77ef266e244b47779bd0361a1dcab124";
